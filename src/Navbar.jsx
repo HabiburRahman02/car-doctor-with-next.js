6d747bea1b899ef49edf156d817a7f3302eb52b7
Habibur Rahman <habibur88454@gmail.com>
2025-03-22 14:48:16 +0600

    services page added

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,14 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
+    const links = <>
+        <li><Link href='/'>Home</Link></li>
+        <li><Link href='/about'>About</Link></li>
+        <li><Link href='/services'>Services</Link></li>
+        <li><Link href='/contact'>Contact</Link></li>
+        <li><Link href='/'>Home</Link></li>
+    </>
     return (
         <div>
             <div className="navbar bg-base-100 shadow-sm">
@@ -12,32 +20,14 @@ const Navbar = () => {
                         <ul
                             tabIndex={0}
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            {links}
                         </ul>
                     </div>
                     <a className="btn btn-ghost text-xl">daisyUI</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {links}
                     </ul>
                 </div>
                 <div className="navbar-end">
